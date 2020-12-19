9ce782ec3b6d0751e51f50a29493dee1cad99389
piteli <rushwender@gmail.com>
2020-12-19 13:53:00 +0800

    updatedf

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, './dist/okayID')));
+app.use(express.static(path.join(__dirname, 'www')));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/okayID/'}
-  );
+    res.sendFile('index.html', {root: 'www/'}
+);
   });
   
 const port = process.env.PORT || 8080;
